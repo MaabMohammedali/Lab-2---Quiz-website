33a04d7bba21a737b91d5f93ec83a49caf1926d1
MaabMohammedali <maab.mohammedali0200@stud.hkr.se>
2024-01-02 18:20:34 +0100

    Add code to show error messege texts

---
+++ Maab.js
@@ -0,0 +1,26 @@
+function displayErrorMessage(message) {
+    document.getElementById('error-message').textContent = message;
+}
+function formValidate() {
+    // this function worked once click on submit button
+    document.getElementById('error-message').textContent = '';
+     // VALID NAME
+   // I use this link to solve this part :https://www.codexworld.com/how-to/validate-first-last-name-with-regular-expression-using-javascript/
+    var firstName = document.getElementById('firstName').value; //  method to access the input field by its ID
+    var lastName = document.getElementById('lastName').value;
+    var nameRegex = /^[A-Za-z]+$/; // to check if both first name and last name contain only letters
+
+    if (!nameRegex.test(firstName) || !nameRegex.test(lastName)) {
+        displayErrorMessage('Try again, only letters are accepted');
+        return;
+    }
+
+   // VALID EMAIL
+    var email = document.getElementById('email').value;
+    var emailRegex= /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // to check if the email has a valid format
+
+    if (!emailRegex.test(email)) {
+        displayErrorMessage('Please enter a valid email address');
+        return;
+    }
+}
