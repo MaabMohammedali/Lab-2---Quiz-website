b5eb4bba28579d8851bb3fc3d76f3f946e20aac9
MaabMohammedali <maab.mohammedali0200@stud.hkr.se>
2024-01-09 17:41:02 +0100

    Use local storage to save the created quiz

--- Own.js
+++ Own.js
@@ -11,5 +11,19 @@ function addNewQuestion() {
         type: answerType,
         answers: possibleAnswers,
         correctAnswers: correctAnswers
+
     };
+    // Save the question to the local storage
+    storeQuestionInLocalStorage(question);
+}
+// Use local storage to save the created quiz on the user's browser.
+function storeQuestionInLocalStorage(question) {
+   // retrieves the current state of the quiz from the browser's local storage
+    var quiz = JSON.parse(localStorage.getItem('quiz')) || [];
+
+    // Add the new question to the quiz
+    quiz.push(question);
+
+    // Save the updated quiz to local storage
+    localStorage.setItem('quiz', JSON.stringify(quiz));
 }
