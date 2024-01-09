0868725089a987b637133ebd3502db2d44fec0d1
MaabMohammedali <maab.mohammedali0200@stud.hkr.se>
2024-01-09 17:29:27 +0100

    Add possible answers to each question through text Define the correct answer by the user Define the type of answers

--- Own.js
+++ Own.js
@@ -1,8 +1,15 @@
-function addQuestion() {
-    // Get values from 
+function addNewQuestion() {
+    // Get values from the form
     var questionText = document.getElementById('question-text').value;
-     // Create an object 
+    var answerType = document.getElementById('answer-type').value;
+    var possibleAnswers = document.getElementById('answers').value.split('\n');
+    var correctAnswers = document.getElementById('correct-answer').value.split(',');
+
+  // Create an object 
     var question = {
         text: questionText,
+        type: answerType,
+        answers: possibleAnswers,
+        correctAnswers: correctAnswers
     };
 }
