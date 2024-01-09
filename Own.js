88aa1030613ba0fe7b358cd8d30c2e6101e4ac0b
MaabMohammedali <maab.mohammedali0200@stud.hkr.se>
2024-01-09 17:17:43 +0100

    Create function to add new questions to the quiz

---
+++ Own.js
@@ -0,0 +1,8 @@
+function addQuestion() {
+    // Get values from 
+    var questionText = document.getElementById('question-text').value;
+     // Create an object 
+    var question = {
+        text: questionText,
+    };
+}
